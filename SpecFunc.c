8be905dd46f4d5b5ac41a37df417c4b812c7bffa
Jymy <basmajymy@gmail.com>
2023-04-25 17:21:58 -0700

    SpecFunc.c

--- SpecFunc.c
+++ SpecFunc.c
@@ -13,6 +13,27 @@ int p_char(char a)
 }
 
 /**
+ * p_number - function
+ * @num: int
+ * Return: Always(0)
+ */
+
+void p_number(int num)
+{
+unsigned int i;
+if (num < 0)
+{
+i = -num;
+p_char('-');
+}
+else
+i = num;
+if (i / 10)
+p_number(i / 10);
+p_char((i % 10) + '0');
+}
+
+/**
  * p_string - A function to print a whole string.
  * @str: the the string
  *
@@ -26,18 +47,18 @@ int p_string(char *str)
 
 /**
 * num_len - function
-*@n: int
+*@num: int
 *Return: Always(0)
 */
 
-int num_len(int n)
+int num_len(int num)
 {
-int len = 0;
-while (n / 10 != 0)
-{
-len ++;
-n /= 10;
-}
+int len = 1;
+p_number(num);
+if (num < 0)
+len++;
+while (num /= 10)
+len++;
 return (len);
 }
 
